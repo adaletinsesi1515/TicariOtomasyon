6ff838e4e13650fa4e44c038dacb89e4fbb872d7
adaletinsesi1515 <memduhgumen@gmail.com>
2022-06-03 00:40:10 +0300

    Kategori listeleme ve menüler tamamlandı

---
+++ TicariOtomasyon/Controllers/KategoriController.cs
@@ -0,0 +1,21 @@
+﻿using MVCOnlineOtomasyon.Models.Sınıflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace TicariOtomasyon.Controllers
+{
+    public class KategoriController : Controller
+    {
+        Context c = new Context();
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            var degerler = c.Kategoris.ToList();
+            return View(degerler);
+        }
+    }
+}
