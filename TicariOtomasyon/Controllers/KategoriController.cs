5a7ef0928e395f5af89d4b4e46d28d1c6b5c4e5c
adaletinsesi1515 <memduhgumen@gmail.com>
2022-06-08 00:19:12 +0300

    Kategori bölümü tamamlandı

--- TicariOtomasyon/Controllers/KategoriController.cs
+++ TicariOtomasyon/Controllers/KategoriController.cs
@@ -17,5 +17,46 @@ namespace TicariOtomasyon.Controllers
             var degerler = c.Kategoris.ToList();
             return View(degerler);
         }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            c.Kategoris.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("Index");            
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategoriSil = c.Kategoris.Find(id);
+            c.Kategoris.Remove(kategoriSil);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategoriUpdate = c.Kategoris.Find(id);
+            return View("KategoriGetir", kategoriUpdate);            
+        }
+
+        [HttpPost]
+        public ActionResult KategoriGuncelleme(Kategori k)
+        {
+            var ktgr = c.Kategoris.Find(k.KategoriID);
+            ktgr.KategoriAd = k.KategoriAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
